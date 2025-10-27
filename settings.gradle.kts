9195bd96fede6df3189e03f2be30d22f0aa6192b
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-10-27 08:57:13 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Projecten f1 criptografia i socols ra5 i ra3 a2"
+include(":app")
