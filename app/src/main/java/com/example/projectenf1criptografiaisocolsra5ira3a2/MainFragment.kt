fbe60f6bf675c009e31f61612e369a6aea5888fb
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-11-06 09:54:28 +0100

    first commit

---
+++ app/src/main/java/com/example/projectenf1criptografiaisocolsra5ira3a2/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.example.projectenf1criptografiaisocolsra5ira3a2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.projectenf1criptografiaisocolsra5ira3a2.databinding.FragmentMainBinding
+import io.socket.client.IO;
+import io.socket.client.Socket;
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMainBinding.inflate(inflater)
+
+        binding.button.setOnClickListener {
+            try {
+                val text = binding.editTextText.text.toString()
+
+                val mSocket: Socket =IO.socket("http://localhost:3000");
+
+                mSocket.connect()
+
+                mSocket.emit("registerPlatform", "mobile");
+
+                mSocket.emit("validarCodi",text)
+
+                Toast.makeText(context, "codi enviat", Toast.LENGTH_SHORT).show()
+
+            }catch (e: Exception){
+                Toast.makeText(context, "error al enviar el codi", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return binding.root
+    }
+}
