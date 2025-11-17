a8b44c224e04286c21a27f2c6c185a5fd318dca3
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-11-17 09:50:37 +0100

    millora visual

--- app/src/main/java/com/example/projectenf1criptografiaisocolsra5ira3a2/MainFragment.kt
+++ app/src/main/java/com/example/projectenf1criptografiaisocolsra5ira3a2/MainFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.projectenf1criptografiaisocolsra5ira3a2.databinding.FragmentMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import org.json.JSONObject
@@ -17,6 +20,13 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentMainBinding.inflate(inflater)
+
+        val toolbar = view?.findViewById<MaterialToolbar>(R.id.topAppBar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        toolbar?.title = "Validador de codi"
+        toolbar?.setTitleTextColor(ContextCompat.getColor(requireContext(), android.R.color.black))
+
 
         binding.button.setOnClickListener {
             try {
